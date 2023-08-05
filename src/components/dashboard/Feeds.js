7a8e1894686624112227912f40a0fe155833c03e
Jo Eunsang <sang11032@gmail.com>
2023-08-05 21:10:39 +0900

    merge를 위한 로컬 수정4

--- src/components/dashboard/Feeds.js
+++ src/components/dashboard/Feeds.js
@@ -11,76 +11,76 @@ import {
 
 const FeedData = [
   {
-    title: "Cras justo odio",
+    title: "공지사항1",
     icon: "bi bi-bell",
     color: "primary",
-    date: "6 minute ago",
+    date: "1 minute ago",
   },
   {
-    title: "New user registered.",
+    title: "공지사항2",
     icon: "bi bi-person",
     color: "info",
     date: "6 minute ago",
   },
   {
-    title: "Server #1 overloaded.",
+    title: "공지사항3",
     icon: "bi bi-hdd",
     color: "danger",
-    date: "6 minute ago",
+    date: "19 minute ago",
   },
   {
-    title: "New order received.",
+    title: "공지사항4",
     icon: "bi bi-bag-check",
     color: "success",
-    date: "6 minute ago",
+    date: "34 minute ago",
   },
   {
-    title: "Cras justo odio",
+    title: "공지사항5",
     icon: "bi bi-bell",
     color: "dark",
-    date: "6 minute ago",
+    date: "47 minute ago",
   },
   {
-    title: "Server #1 overloaded.",
+    title: "공지사항6",
     icon: "bi bi-hdd",
     color: "warning",
-    date: "6 minute ago",
+    date: "1 hours ago",
   },
 ];
 
 const Feeds = () => {
   return (
-    <Card>
-      <CardBody>
-        <CardTitle tag="h5">공지사항</CardTitle>
-        <CardSubtitle className="mb-2 text-muted" tag="h6">
-          공지사항입니다
-        </CardSubtitle>
-        <ListGroup flush className="mt-4">
-          {FeedData.map((feed, index) => (
-            <ListGroupItem
-              key={index}
-              action
-              href="/"
-              tag="a"
-              className="d-flex align-items-center p-3 border-0"
-            >
-              <Button
-                className="rounded-circle me-3"
-                size="sm"
-                color={feed.color}
-              >
-                <i className={feed.icon}></i>
-              </Button>
-              {feed.title}
-              <small className="ms-auto text-muted text-small">
-                {feed.date}
-              </small>
-            </ListGroupItem>
-          ))}
-        </ListGroup>
-      </CardBody>
-    </Card>
+      <Card>
+        <CardBody>
+          <CardTitle tag="h5">공지사항</CardTitle>
+          <CardSubtitle className="mb-2 text-muted" tag="h6">
+            공지사항입니다
+          </CardSubtitle>
+          <ListGroup flush className="mt-4">
+            {FeedData.map((feed, index) => (
+                <ListGroupItem
+                    key={index}
+                    action
+                    href="/"
+                    tag="a"
+                    className="d-flex align-items-center p-3 border-0"
+                >
+                  <Button
+                      className="rounded-circle me-3"
+                      size="sm"
+                      color={feed.color}
+                  >
+                    <i className={feed.icon}></i>
+                  </Button>
+                  {feed.title}
+                  <small className="ms-auto text-muted text-small">
+                    {feed.date}
+                  </small>
+                </ListGroupItem>
+            ))}
+          </ListGroup>
+        </CardBody>
+      </Card>
   );
 };
 
