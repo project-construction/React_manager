0a3212cede459477a8c47a6c15840dcd22630f8e
Jo Eunsang <sang11032@gmail.com>
2023-08-06 18:24:26 +0900

    라우터 이동을 위한 경로 재설정

--- src/App.js
+++ src/App.js
@@ -1,10 +1,34 @@
-import { useRoutes } from "react-router-dom";
-import Themeroutes from "./routes/Router";
-
+import {Navigate} from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
+import Alerts from "./views/ui/Alerts";
+import FullLayout from "./layouts/FullLayout";
+import Starter from "./views/Starter";
+import About from "./views/About";
+import Badges from "./views/ui/Badges";
+import Buttons from "./views/ui/Buttons";
+import Cards from "./views/ui/Cards";
+import Grid from "./views/ui/Grid";
+import Tables from "./views/ui/Tables";
+import Forms from "./views/ui/Forms";
+import Breadcrumbs from "./views/ui/Breadcrumbs";
 const App = () => {
-  const routing = useRoutes(Themeroutes);
-
-  return <div className="dark">{routing}</div>;
-};
+  return (
+      <Routes>
+        <Route path="/" element={<FullLayout />}>
+          <Route index element={<Navigate to="/starter" />} />
+          <Route path="/starter" element={<Starter />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/alerts" element={<Alerts />} />
+          <Route path="/badges" element={<Badges />} />
+          <Route path="/buttons" element={<Buttons />} />
+          <Route path="/cards" element={<Cards />} />
+          <Route path="/grid" element={<Grid />} />
+          <Route path="/table" element={<Tables />} />
+          <Route path="/forms" element={<Forms />} />
+          <Route path="/breadcrumbs" element={<Breadcrumbs />} />
+        </Route>
+      </Routes>
+  );
+  };
 
 export default App;
