0ce9986a3287b2848d4852f855ee187426188fc4
bnh <skagus159632@naver.com>
2023-08-30 17:33:55 +0900

    노동자 컨텐츠 차트 표시

--- src/views/Starter.js
+++ src/views/Starter.js
@@ -8,6 +8,7 @@ import bg1 from "../assets/images/bg/bg1.jpg";
 import bg2 from "../assets/images/bg/bg2.jpg";
 import bg3 from "../assets/images/bg/bg3.jpg";
 import bg4 from "../assets/images/bg/bg4.jpg";
+import {useState} from "react";
 
 const BlogData = [
   {
@@ -45,6 +46,18 @@ const BlogData = [
 ];
 
 const Starter = () => {
+  const [send, setSend] = useState({
+    doorlock : 0,
+    hammering : 0,
+    simon : 0,
+    trafficLight : 0,
+    catchMole : 0,
+    numberPuzzle : 0,
+    depression : 0,
+    anxiety : 0,
+    stress : 0,
+    nback : 0
+  });
   return (
     <div>
       {/***Top Cards***/}
@@ -52,7 +65,7 @@ const Starter = () => {
       {/***Sales & Feed***/}
       <Row>
         <Col sm="6" lg="6" xl="7" xxl="8">
-          <SalesChart />
+          <SalesChart send={send}/>
         </Col>
         <Col sm="6" lg="6" xl="5" xxl="4">
           <Feeds />
@@ -61,7 +74,7 @@ const Starter = () => {
       {/***Table ***/}
       <Row>
         <Col lg="12">
-          <ProjectTables />
+          <ProjectTables setSend={setSend}/>
         </Col>
       </Row>
       {/***Blog Cards***/}
