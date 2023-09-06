2c5aa0fdc2f014c102c0858ac9352e535a0f423d
yyechan0602 <yyechan0602@naver.com>
2023-09-06 16:43:11 +0900

    fix permising function

--- src/components/dashboard/RecognizeTable.js
+++ src/components/dashboard/RecognizeTable.js
@@ -1,36 +1,41 @@
-import {Button, Card, CardBody, CardTitle, Table} from "reactstrap";
-import user1 from "../../assets/images/users/user1.jpg";
-import user2 from "../../assets/images/users/user2.jpg";
-import user3 from "../../assets/images/users/user3.jpg";
-import user4 from "../../assets/images/users/user4.jpg";
-import user5 from "../../assets/images/users/user5.jpg";
-
-const tableData = [
-    {
-        avatar: user1,
-        name: "노동자1",
-        email: "hgover@gmail.com",
-        address: "서울시 송파구 ",
-        gender: "female",
-        phone: "010-1234-5678",
-        team: "정왕1팀",
-    },
-    {
-        avatar: user1,
-        name: "노동자2",
-        email: "asdsr@gmail.com",
-        address: "서울시 마포구",
-        gender: "male",
-        phone: "010-2412-1424",
-        team: "정왕 2팀",
-    }
-];
-
-const ButtonAction = (event) => {
-
-};
+import React, {useState, useEffect} from 'react';
+import {Button, Card, CardBody, CardTitle, Table} from 'reactstrap';
 
 const ProjectTables = () => {
+    const [tableData, setTableData] = useState([]);
+
+    const requestOptions = {
+        method: 'GET',
+        headers: {
+            'Content-Type': 'application/json', // 요청의 Content-Type 설정
+        },
+        mode: 'cors', // CORS 활성화
+    };
+
+    useEffect(() => {
+        // API에서 노동자 정보 가져오기 (GET 방식)
+        fetch(`https://port-0-spring-eu1k2llldpju8v.sel3.cloudtype.app/worker/wait`, requestOptions)
+            .then(response => response.json())
+            .then(data => {
+                // 가져온 데이터로 tableData 상태 업데이트
+                setTableData(data);
+            })
+            .catch(error => {
+                console.error('노동자 정보를 가져오는 중 오류 발생:', error);
+            });
+
+    }, []);
+
+    const handleApproval = (email) => {
+        // 승인 버튼 동작 처리
+        // 이메일에 해당하는 노동자를 승인하는 API 호출
+    };
+
+    const handleRejection = (email) => {
+        // 거부 버튼 동작 처리
+        // 이메일에 해당하는 노동자를 거부하는 API 호출
+    };
+
     return (
         <div>
             <Card>
@@ -40,48 +45,42 @@ const ProjectTables = () => {
                         <thead>
                         <tr>
                             <th>노동자명</th>
-
-                            <th>주 소</th>
-                            <th>성 별</th>
+                            <th>소속</th>
+                            <th>생년월일</th>
+                            <th>성별</th>
                             <th>연락처</th>
-                            <th>소 속</th>
-
+                            <th>주소</th>
                             <th>승인 여부</th>
                         </tr>
                         </thead>
                         <tbody>
                         {tableData.map((tdata, index) => (
                             <tr key={index} className="border-top">
-                                <td>
-                                    <div className="d-flex align-items-center p-2">
-                                        <img
-                                            src={tdata.avatar}
-                                            className="rounded-circle"
-                                            alt="avatar"
-                                            width="45"
-                                            height="45"
-                                        />
-                                        <div className="ms-3">
-                                            <h6 className="mb-0">{tdata.name}</h6>
-                                            <span className="text-muted">{tdata.email}</span>
-                                        </div>
-                                    </div>
-                                </td>
-                                <td>{tdata.address}</td>
+                                <td>{tdata.name}</td>
+                                <td>{tdata.team}</td>
+                                <td>{tdata.birth}</td>
                                 <td>{tdata.gender}</td>
                                 <td>{tdata.phone}</td>
-                                <td>{tdata.team}</td>
-
-                                <td>{}
-                                <Button outline color="success" variant="contained" size="large"
-                                         onClick={ButtonAction}>
-                                    승인
-                                </Button>
-
-                                <Button outline color="danger" variant="contained" size="large"
-                                        onClick={ButtonAction}>
-                                    거부
-                                </Button>
+                                <td>{tdata.address}</td>
+                                <td>
+                                    <Button
+                                        style={{marginRight: '10px'}}
+                                        color="btn btn btn-outline-primary"
+                                        variant="contained"
+                                        size="large"
+                                        onClick={() => handleApproval(tdata.email)}
+                                    >
+                                        승인
+                                    </Button>
+                                    <Button
+                                        padding="10px"
+                                        color="btn btn btn-outline-danger"
+                                        variant="contained"
+                                        size="large"
+                                        onClick={() => handleRejection(tdata.email)}
+                                    >
+                                        거부
+                                    </Button>
                                 </td>
                             </tr>
                         ))}
