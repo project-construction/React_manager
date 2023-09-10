efe1639301690341d5926991eb5247fae744e1f7
abbiddo@naver.com <abbiddo@naver.com>
2023-09-10 21:40:15 +0900

    modify schedule update and create schedule delete

--- src/views/ui/Cards.js
+++ src/views/ui/Cards.js
@@ -1,20 +1,17 @@
-import React, {useRef, useState} from "react";
+import React, {useEffect, useRef, useState} from "react";
 import DatePicker from "react-datepicker";
 import dayjs from "dayjs";
 import ko from "date-fns/locale/ko";
 import "react-datepicker/dist/react-datepicker.css";
 import "./Cards.css";
+import {Button} from "reactstrap";
 
 
 const Cards = ({onCreate}) => {
     // 일정 추가
-    const [schedules, setSchedules] = useState([{ schedule: "", startTime: null, time : "" }]);
+    const [schedules, setSchedules] = useState([{ id: 0, content: "", startTime: null, time : "" }]);
     // 선택한 날짜를 로컬 상태로 저장
     const [selectedDate, setSelectedDate] = useState(null);
-    const [date, setDate] = useState(null);
-    // 일정 칸이 비어있는지 확인
-    const [isEmpty, setIsEmpty] = useState(false);
-
     // 현재 시간을 가져오는 함수
     const getCurrentTime = () => new Date();
     const scheduleInput=useRef();
@@ -22,51 +19,99 @@ const Cards = ({onCreate}) => {
 
     // 일정 추가 버튼 누르면 일정 추가
     const handleAddSchedule = () => {
-        setSchedules([...schedules, { schedule: "", startTime: null }]);
+        setSchedules([...schedules, { id: 0, content: "", startTime: null, time : ""  }]);
     };
 
-    const handleDateChange = (date) => {
-        setSelectedDate(date); // 선택한 날짜를 로컬 상태에 저장
-        const year = selectedDate.getFullYear().toString();
-        const month = (selectedDate.getMonth()+1).toString().padStart(2, '0');
-        const day = selectedDate.getDay().toString().padStart(2, '0');
-        setDate(year+'-'+month+'-'+day);
+    // 삭제하기
+    const handleDeleteSchedule = (index) => {
+        fetch(`http://localhost:8080/schedule/delete/${schedules[index].id}`, {
+            method: 'DELETE',
+            mode: 'cors'
+        });
+
+        const updatedSchedules = schedules.filter((_, i) => i !== index);
+        setSchedules(updatedSchedules);
+    };
+
+    useEffect(function () {
+        const dateback = async (year, month, day) => {
+            try {
+                const date = `${year}-${month}-${day}`;
+
+                const response = await fetch(`http://localhost:8080/schedule/find/${date}`, {
+                    method: 'GET',
+                    mode: 'cors'
+
+                });
+
+                if (!response.ok) {
+                    throw new Error('서버 응답이 실패하였습니다.');
+                }
+
+                const data = await response.json();
+
+                // 중복된 데이터를 제외한 새로운 스케줄 목록
+                const newSchedules = data.map(item => ({
+                    id: item.id,
+                    content: item.content,
+                    startTime: new Date(item.date + ' ' + item.time),
+                    time: item.time
+                }));
+
+                // 새로운 스케줄을 기존 스케줄에 추가
+                setSchedules(newSchedules);
+
+            } catch (error) {
+                console.error("Error fetching data:", error);
+            }
+        }
+
+        if (selectedDate !== null) {
+            const year = selectedDate.getFullYear().toString();
+            const month = (selectedDate.getMonth() + 1).toString().padStart(2, '0');
+            const day = selectedDate.getDate().toString().padStart(2, '0');
+            dateback(year, month, day);
+        }
+    }, [selectedDate]);
+
+
+    const handleDateChange = async (date) => {
+
+        if (date === null) {
+            setSelectedDate(null);
+        }
+
+        else {
+            setSelectedDate(date); // 선택한 날짜를 로컬 상태에 저장
+        }
     };
 
     // 저장하기
     const handleSubmit=async ()=>{
-
-
-
-        console.log(schedules[0].startTime);
-
-        for(let i=0;i<schedules.length;i++){
-            const hour = schedules[i].startTime.getHours().toString().padStart(2, '0');
-            const minute = schedules[i].startTime.getMinutes().toString().padStart(2, '0');
-            schedules[i].time = hour + ':' + minute;
-        }
-
-        console.log(schedules);
-
+        const year = selectedDate.getFullYear().toString();
+        const month = (selectedDate.getMonth() + 1).toString().padStart(2, '0');
+        const day = selectedDate.getDate().toString().padStart(2, '0');
+        const date = `${year}-${month}-${day}`;
 
         if(selectedDate===null){
             alert("날짜를 선택해주세요")
             return;
         }
         for (let i = 0; i < schedules.length; i++) {
-            if(schedules[i].schedule.length<1){
+            if (schedules[i].content.length < 1) {
                 alert("일정을 입력해주세요")
                 scheduleInput.current.focus();
-                setIsEmpty(true);
                 return;
-            }
-            else if(schedules[i].startTime==null){
+            } else if (schedules[i].startTime == null) {
                 alert("시간을 입력해주세요");
                 return;
             }
         }
-        if(isEmpty){
-            return;
+
+        for(let i=0;i<schedules.length;i++){
+            const hour = schedules[i].startTime.getHours().toString().padStart(2, '0');
+            const minute = schedules[i].startTime.getMinutes().toString().padStart(2, '0');
+            schedules[i].time = hour + ':' + minute;
         }
 
         const requestOptions = {
@@ -78,7 +123,7 @@ const Cards = ({onCreate}) => {
             mode: 'cors'
         };
 
-        fetch('http://localhost:8080/notice/write', requestOptions)
+        fetch('http://localhost:8080/schedule/add', requestOptions)
             .then(response => response)
             .then(data => {
                 console.log('submitted:', data);
@@ -87,12 +132,13 @@ const Cards = ({onCreate}) => {
                 console.error('Error submitting:', error);
             });
 
+        setSelectedDate(null);
 
 
         alert("저장성공!")
         setSchedules([
             {
-                schedule: "",
+                content: "",
                 startTime: null,
             }
         ]);
@@ -106,7 +152,6 @@ const Cards = ({onCreate}) => {
                             dateFormat="yyyy년 MM월 dd일 eee요일"
                             dateFormatCalendar="yyyy년 MM월"
                             locale={ko}
-                            timeCaption="날짜 선택"
                             timeCaption="날짜 선택"
                             placeholderText="날짜 선택"
                             selected={selectedDate}
@@ -127,48 +172,58 @@ const Cards = ({onCreate}) => {
                     <div>
                         <input className={"schedule"}
                                ref={scheduleInput}
-                                placeholder="일정"
-                                value={schedule.schedule}
-                                onChange={(e) => {
-                                    const updatedSchedules = [...schedules];
-                                    updatedSchedules[index].schedule = e.target.value;
-                                    setSchedules(updatedSchedules);
-                                }}
-                            />
+                               placeholder="일정"
+                               value={schedule.content}
+                               onChange={(e) => {
+                                   const updatedSchedules = [...schedules];
+                                   updatedSchedules[index].content = e.target.value;
+                                   setSchedules(updatedSchedules);
+                               }}
+                        />
                     </div>
-                        <div>
-                            <DatePicker className={"start_time"}
-                                ref={startTimeInput}
-                                selected={schedule.startTime}
-                                onChange={(time) => {
-                                    const updatedSchedules = [...schedules];
-                                    updatedSchedules[index].startTime = time;
-                                    setSchedules(updatedSchedules);
-                                }}
-                                locale={ko}
-                                showTimeSelect
-                                showTimeSelectOnly
-                                timeIntervals={30}
-                                minTime={getCurrentTime()}
-                                maxTime={dayjs().endOf("day").toDate()}
-                                timeCaption="Time"
-                                dateFormat="aa h:mm ~"
-                                placeholderText="시작 시간"
-                                className="start_time"
-                            />
+                    <div>
+                        <DatePicker className={"start_time"}
+                                    ref={startTimeInput}
+                                    selected={schedule.startTime}
+                                    onChange={(time) => {
+                                        const updatedSchedules = [...schedules];
+                                        updatedSchedules[index].startTime = time;
+                                        setSchedules(updatedSchedules);
+                                    }}
+                                    locale={ko}
+                                    showTimeSelect
+                                    showTimeSelectOnly
+                                    timeIntervals={30}
+                                    minTime={getCurrentTime()}
+                                    maxTime={dayjs().endOf("day").toDate()}
+                                    timeCaption="Time"
+                                    dateFormat="aa h:mm ~"
+                                    placeholderText="시작 시간"
+                                    className="start_time"
+                        />
 
-                        </div>
+                        <Button
+                            onClick={() => handleDeleteSchedule(index)}
+                            name="delete-schedule"
+                            color ="danger"
+                            style = {{marginLeft:"10px"}}
+                        >
+                            삭제
+                        </Button>
+                    </div>
                 </div>
             ))}
 
 
             <div>
-                <button onClick={handleAddSchedule}
+                <Button onClick={handleAddSchedule}
                         name="schedule-bot"
-                        >일정 추가</button>
-                <button onClick={handleSubmit}
+                        style = {{marginRight:"10px"}}
+                >일정 추가</Button>
+                <Button onClick={handleSubmit}
                         name="save-bot"
-                        >저장</button>
+                        color = "primary"
+                >저장</Button>
             </div>
         </div>
     );
